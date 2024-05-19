53e255f5ac0fd6df9b2f75d6c7387ac4adb9477c
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-05-19 12:07:23 +0330

    feature: implement multer logic

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const productsController = require("../controllers/product");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // Destination folder for uploaded files
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Use original file name
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post("/", upload.array("images", 5), productsController.createProduct);
 
 router
   .route("/")
   .get(productsController.getAllProducts)
-  .post(productsController.createProduct)
-  .patch(productsController.updateProduct)
+  .patch(upload.array("images", 5), productsController.updateProduct)
   .delete(productsController.deleteProduct);
 
 router.route("/:id").get(productsController.getProductById);
